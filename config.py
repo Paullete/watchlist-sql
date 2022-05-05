5a0efcef58bb62248fd8fed6620c479236c251d3
paullete <paulettenereah3@gmail.com>
2022-05-05 15:40:03 +0300

    sql

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 class Config:
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=aabb0c7ca8b510be8e9e03a4e8ffd9aa'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dell:Dell@localhost/watchlist'
 
 
 class ProdConfig(Config):
